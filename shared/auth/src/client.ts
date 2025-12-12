d22c02ef364facdbc717ad398352f0c96a0b52b8
Maksim Levin <sim.lev03@gmail.com>
2025-12-12 19:42:10 +0300

    Почти доделанное разграничение по ролям

--- shared/auth/src/client.ts
+++ shared/auth/src/client.ts
@@ -1,5 +1,7 @@
-import { admin } from "better-auth/plugins";
 import { createAuthClient as createBetterAuthClient } from "better-auth/react";
+import { adminClient } from "better-auth/client/plugins"
+import { ac, roles } from "./permissionControl"
+
 
 export interface AuthClientOptions {
   apiBaseUrl: string;
@@ -13,6 +15,9 @@ export const createAuthClient = ({
   createBetterAuthClient({
     baseURL: `${apiBaseUrl}${apiBasePath}/auth`,
     plugins: [
-      admin(),
+      adminClient({
+        ac,
+        roles
+      }),
     ]
   });
