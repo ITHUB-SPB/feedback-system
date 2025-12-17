b9adc68a97d20cf81074fcdea344ff19174d59b1
Lex Trofimov <daslef93@gmail.com>
2025-12-17 12:16:24 +0300

    fix(auth)

---
+++ apps/server/src/auth.ts
@@ -0,0 +1,16 @@
+import { createAuth, type AuthInstance } from "@shared/auth";
+import { db } from "@shared/database";
+import { trustedOrigins } from "./cors";
+import { env } from "./env";
+
+console.log(trustedOrigins, env)
+
+const auth: AuthInstance = createAuth({
+  trustedOrigins,
+  serverUrl: env.PUBLIC_SERVER_URL,
+  apiPath: "/api",
+  authSecret: env.SERVER_AUTH_SECRET,
+  db,
+})
+
+export default auth
